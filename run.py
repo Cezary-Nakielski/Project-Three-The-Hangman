d0a815009fc9a600350d69d4b7ae4ee36aebbed7
Cezary-Nakielski <cezarnakielski@gmail.com>
2021-11-30 22:32:15 +0000

    MAINT: Add an if statement acting on users input which he previously tried

--- run.py
+++ run.py
@@ -13,19 +13,19 @@ def game(secret_word):
     attempt_letter = []
     attempts = 6
     word_display = len(secret_word) * "_"
-    guess = input("Guess the word:")
+    guess = input("Guess the word or a letter contained in the word:")
     if guess == secret_word:
         print("You Won!")
-    else:
-        print("You Lost!")
-
-    if guess not in word
+    # else:
+    #     print("You Lost!")
+    if guess not in word:
         attempt_word.append(guess)
         attempts -= 1
-
-    if guess is not word
+    if guess is not word:
         attempt_letter.append(guess)
         attempts -= 1
+    if guess in attempt_letter:
+        print("You already tried that.")
 
 def main():
     secret_word = secret_random_word()
