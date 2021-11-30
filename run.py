e0d141d0c6a6baff640f15cbf8fe221087d2d940
Cezary-Nakielski <cezarnakielski@gmail.com>
2021-11-30 20:13:15 +0000

    FIX: Rework the 'game' function

--- run.py
+++ run.py
@@ -7,25 +7,9 @@ def secret_random_word():
     return secret_word
 
 
-def pass_word():
-    secret_word = secret_random_word()
-    game(secret_word)
-
-
-pass_word()
-
-
-game_score = False
-
-
 def game(secret_word):
-    while game_score:
-        guess = input("Guess the word:")
-
+    guess = input("Guess the word:")
     if guess == secret_word:
-        not game_score
-
-    if game_score:
-        print("Win")
+        print("You Won!")
     else:
-        print("Defeat")
+        print("You Lost!")
