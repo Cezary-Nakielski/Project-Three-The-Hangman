21f29188f56714c8c50babd81028d66f6f7861d9
Cezary-Nakielski <cezarnakielski@gmail.com>
2021-11-30 23:22:40 +0000

    MAINT: Add an if statement inside the 'game' function, which relates to user inputting a letter

--- run.py
+++ run.py
@@ -15,18 +15,25 @@ def game(secret_word):
     word_display = len(secret_word) * "_"
     while not game_score:
         guess = input("Guess the word or a letter contained in the word:")
+        if guess in attempt_letter:
+            print("You already tried that.")
+        elif guess not in secret_word:
+            attempt_letter.append(guess)
+            attempts -= 1
+            print(f"{guess} is not contained in the word.")
+        else:
+            attempt_letter.append(guess)
+            print(f"{guess} is contained in the word.")
+
+            
     # if guess == secret_word:
     #     print("You Won!")
     # else:
     #     print("You Lost!")
-    # if guess not in word:
-    #     attempt_word.append(guess)
-    #     attempts -= 1
-    # if guess is not word:
+    # if guess is not secret_word:
     #     attempt_letter.append(guess)
     #     attempts -= 1
-    # if guess in attempt_letter:
-    #     print("You already tried that.")
+
 
 def main():
     secret_word = secret_random_word()
