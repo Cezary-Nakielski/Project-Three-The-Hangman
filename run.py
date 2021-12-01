f8e4fb0f7a8f2b9c9b401752fa935261c44dac9a
Cezary-Nakielski <cezarnakielski@gmail.com>
2021-12-01 11:21:26 +0000

    FIX & REFACTOR: Correct indentations and operators inside the 'game' function

--- run.py
+++ run.py
@@ -15,28 +15,30 @@ def game(secret_word):
     word_display = len(secret_word) * "_"
     while not game_score:
         guess = input("Guess the word or a letter contained in the word:")
-        if guess in attempt_letter:
-            print(f"You already tried {guess} before.")
-        elif guess not in secret_word:
-            attempt_letter.append(guess)
-            attempts -= 1
-            print(f"{guess} is not contained in the word.")
-        elif:
-            attempt_letter.append(guess)
-            word_display_list = list(word_display)
-            indices = [i for i, letter in enumerate(secret_word)
-                       if letter == guess]
-            for index in indices:
-                word_display_list[index] = guess
-            word_display = "".join(word_display_list)
-            print(f"{guess} is contained in the word.")
-        elif len(secret_word) = len(guess):
+        if len(guess) == 1:
+            if guess in attempt_letter:
+                print(f"You already tried {guess} before.") 
+            elif guess not in secret_word:
+                attempt_letter.append(guess)
+                attempts -= 1
+                print(f"{guess} is not contained in the word.")
+            else:
+                attempt_letter.append(guess)
+                word_display_list = list(word_display)
+                indices = [i for i, letter in enumerate(secret_word)
+                           if letter == guess]
+                for index in indices:
+                    word_display_list[index] = guess
+                word_display = "".join(word_display_list)
+                print(f"{guess} is contained in the word.")
+        elif len(secret_word) == len(guess):
             if guess in attempt_word:
                 print(f"You already tried {guess} before.")
             else:
                 attempt_word.append(guess)
                 attempts -= 1
                 print(f"{guess} is not the word you're looking for.")
+
 
     # if guess == secret_word:
     #     print("You Won!")
